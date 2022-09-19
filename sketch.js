085799b598dbcbb34a52de17562f1796697b0730
rafa <94569976+rafaellebyju@users.noreply.github.com>
2022-09-19 19:13:36 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
-var chao,player;
+var chao,player,inimigo;
 var gameState = 0
 var botaoplay, titulo;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   // 1 = pro lado pro outro , 2 = altura, 3 = largura, 4 = espaçamento
-  chao = createSprite(400, 695, 1800, 50);
+  chao = createSprite(400, 695, 220000, 50);
   player = createSprite(30,650,50,80);
   player.shapeColor = "red"
+
+  //inimigos
+
 
   //criação do menu
   botaoplay = createButton("play")
@@ -17,11 +20,16 @@ function setup() {
   titulo = createElement("h1","INSIRA UM TITULO AQUI!")
   titulo.class("titulo")
   titulo.position(width/2 - 100,height/2 - 100)
+
+
+
 }
 
 function draw() {
   background(0);  
   play();
+  inimigo();
+  
 }
 
 function movPlayer(){
@@ -50,3 +58,20 @@ function play(){
   
 }
 }
+
+function inimigo(){
+  if(frameCount %60 === 0){
+    var inimigo = createSprite(1100,650,50,80);
+    inimigo.shapeColor = "blue"
+    inimigo.x = Math.round(random(800,1100));
+    inimigo.scale = 0.5;
+    inimigo.velocityX = -2
+    inimigo.lifetime = 100;
+
+  }
+  
+}
+
+
+
+
